d1c506c60ed973b30228200b3bc57ef3a4986926
nabinoy <nabinoybaroi007@gmail.com>
2023-12-02 15:56:17 +0530

    text field added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:ytshare/constants/global.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:ytshare/screens/settings.dart';
-import 'package:ytshare/theme/theme_provider.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,8 +12,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   bool isDarkMode = false;
+
+  bool isApiCallProcess = false;
+  String link = "";
+  String password = "";
+  bool isLoading = false;
+  final _formKey = GlobalKey<FormState>();
 
   void toggleTheme() {
     setState(() {
@@ -31,6 +34,21 @@ class _HomeState extends State<Home> {
       systemNavigationBarIconBrightness:
           isDarkMode ? Brightness.light : Brightness.dark,
     ));
+  }
+
+  String extractVideoId(String url) {
+    // Regular expression to match YouTube video ID in the URL
+    RegExp regExp = RegExp(
+        r'^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})');
+
+    // Extract video ID using the regular expression
+    Match? match = regExp.firstMatch(url);
+
+    if (match != null && match.groupCount >= 1) {
+      return match.group(1)!;
+    } else {
+      return 'Invalid YouTube URL';
+    }
   }
 
   @override
@@ -64,23 +82,163 @@ class _HomeState extends State<Home> {
           children: [],
         ),
       ),
-      body: const Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    children: <Widget>[
+                      const Text(
+                        "Random text Lorem ipsum",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      Text(
+                        "Login to your account with some dummy text lorem ipsum dolor.",
+                        style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 24),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter YouTube URL';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              hintText: 'Enter YouTube URL',
+                              hintStyle: const TextStyle(fontSize: 16),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                borderSide: const BorderSide(
+                                  width: 3,
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                ),
+                              ),
+                              filled: true,
+                              contentPadding: const EdgeInsets.all(16),
+                            ),
+                            onChanged: (value) => setState(() {
+                              link = value;
+                            }),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        FocusScopeNode currentFocus = FocusScope.of(context);
+                        if (!currentFocus.hasPrimaryFocus) {
+                          currentFocus.unfocus();
+                        }
+
+                        if (_formKey.currentState!.validate()) {
+                          isLoading = true;
+                          setState(() {
+                            isApiCallProcess = true;
+                          });
+
+                          String youtubeVideoId = extractVideoId(link);
+                          print('YouTube Video ID: $youtubeVideoId');
+
+                          // LoginRequestModel model = LoginRequestModel(
+                          //   email: email,
+                          //   password: password,
+                          // );
+
+                          // APIService.login(model).then(
+                          //   (response) {
+                          //     setState(() {
+                          //       isApiCallProcess = false;
+                          //     });
+                          //     if (response) {
+                          //       SharedService.setSharedHomeAfter(true);
+                          //       Navigator.pushNamedAndRemoveUntil(
+                          //         context,
+                          //         Home.routeName,
+                          //         (route) => false,
+                          //       );
+                          //     } else {
+                          //       isLoading = false;
+                          //       final snackBar = SnackBar(
+                          //         width: double.infinity,
+                          //         dismissDirection: DismissDirection.down,
+                          //         elevation: 0,
+                          //         behavior: SnackBarBehavior.floating,
+                          //         backgroundColor: Colors.transparent,
+                          //         content: DefaultTextStyle(
+                          //           style: const TextStyle(
+                          //             fontFamily: fontRegular,
+                          //           ),
+                          //           child: AwesomeSnackbarContent(
+                          //             title: 'Error!',
+                          //             message:
+                          //                 'Email or password is incorrect, please try again!',
+                          //             contentType: ContentType.failure,
+                          //           ),
+                          //         ),
+                          //       );
+                          //       ScaffoldMessenger.of(context)
+                          //         ..hideCurrentSnackBar()
+                          //         ..showSnackBar(snackBar);
+                          //     }
+                          //   },
+                          // );
+                        }
+                      },
+                      color: Colors.lightBlue[800],
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      child: isLoading
+                          ? const CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "Proceed",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18),
+                            ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          toggleTheme();
-          Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
